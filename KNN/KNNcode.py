fcb3fa45e7ed8c9b6b17d431173aaf46928dc9c4
Simge Mede <109466550+simgemede@users.noreply.github.com>
2024-06-02 14:52:14 +0300

    ing

--- KNN/KNNcode.py
+++ KNN/KNNcode.py
@@ -4,17 +4,17 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-# çıktı = 1 şeker hastası
-# çıktı = 0 sağlıklı
+# output = 1 diabetic
+# output = 0 healthy
 data = pd.read_csv("diabetes.csv")
 data.head()
 print(data)
 
-seker_hastaları = data[data.Outcome == 1]
-saglikli_insanlar = data[data.Outcome == 0]
+diabetic = data[data.Outcome == 1]
+healthy = data[data.Outcome == 0]
 
-plt.scatter(saglikli_insanlar.Age, saglikli_insanlar.Glucose, color="green", label="sağlıklı", alpha=0.4)
-plt.scatter(seker_hastaları.Age, seker_hastaları.Glucose, color="red", label="şeker hastası", alpha=0.4)
+plt.scatter(healthy.Age, healthy.Glucose, color="green", label="healthy", alpha=0.4)
+plt.scatter(diabetic.Age, diabetic.Glucose, color="red", label="diabetic", alpha=0.4)
 plt.xlabel("Age")
 plt.ylabel("Glucose")
 plt.legend()
